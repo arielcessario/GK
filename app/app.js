2290ab21801b2abd8062f410baced9f22f48df7f
arielcessario <arielcessario@gmail.com>
2015-12-15 09:39:04 -0300

    Ariel

--- app/app.js
+++ app/app.js
@@ -40,34 +40,54 @@
             }])
         .controller('AppController', AppController)
         .service('AppService', AppService)
+        .directive('navContacto', NavContacto)
         .run(function ($rootScope) {
             $rootScope.$on('$routeChangeStart', function (e, to) {
+                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-
-                //(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                //        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                //    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                //})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-                //
-                //ga('create', 'UA-56571440-2', 'auto');
-                //ga('send', 'pageview', to);
+                ga('create', 'UA-56571440-3', 'auto');
+                ga('send', 'pageview', to);
             })
         });
 
-    AppController.$inject = ['$location', '$timeout', '$document'];
-    function AppController($location, $timeout, $document) {
+    AppController.$inject = ['$location', '$timeout', '$document', '$window', '$scope', '$http'];
+    function AppController($location, $timeout, $document, $window, $scope, $http) {
 
         var vm = this;
         vm.menu_mobile_open = false;
         vm.goToAnchor = goToAnchor;
         vm.goToProductos = goToProductos;
+        vm.sendMail = sendMail;
+        vm.scroll = 0;
 
-        function goToProductos(){
+        vm.email = '';
+        vm.nombre = '';
+        vm.mensaje = '';
+        vm.asunto = '';
+        vm.enviado = false;
+
+
+        function goToProductos() {
 
             $location.path('/productos');
-            goToAnchor('productos');
+            $timeout(function () {
+                var duration = 1000;
+                var offset = 50; //pixels; adjust for floating menu, context etc
+                //Scroll to #some-id with 30 px "padding"
+                //Note: Use this in a directive, not with document.getElementById
+                var someElement = angular.element(document.getElementById('productos'));
+                $document.scrollToElement(someElement, offset, duration);
+            }, 10);
         }
 
+
+        angular.element($window).bind("scroll", function () {
+            vm.scroll = window.pageYOffset;
+            $scope.$apply();
+        });
 
         function goToAnchor(id) {
             $location.path('/main');
@@ -84,6 +104,32 @@
 
         }
 
+        function sendMail() {
+
+
+            return $http.post('contact.php',
+                {'email': vm.email, 'nombre': vm.nombre, 'mensaje': vm.mensaje, 'asunto': vm.asunto})
+                .success(
+                    function (data) {
+                        console.log(data);
+                        vm.enviado = true;
+                        $timeout(hideMessage, 3000);
+                        function hideMessage() {
+                            vm.enviado = false;
+                        }
+
+                        vm.email = '';
+                        vm.nombre = '';
+                        vm.mensaje = '';
+                        vm.asunto = '';
+
+                        //goog_report_conversion('http://www.ac-desarrollos.com/#');
+                    })
+                .error(function (data) {
+                    console.log(data);
+                });
+        }
+
     }
 
     AppService.$inject = ['$rootScope'];
@@ -96,11 +142,40 @@
             $rootScope.$broadcast('gkradio');
         }
     }
+
+    NavContacto.$inject = ['$timeout', '$document'];
+    function NavContacto($timeout, $document) {
+        return {
+            restrict: 'AE',
+            scope: {},
+            controller: function ($scope, $element, $attrs) {
+
+                $element.bind('click', function () {
+
+                    $timeout(function () {
+                        var duration = 1000;
+                        var offset = 50; //pixels; adjust for floating menu, context etc
+                        //Scroll to #some-id with 30 px "padding"
+                        //Note: Use this in a directive, not with document.getElementById
+                        var someElement = angular.element(document.getElementById('contacto-container'));
+                        $document.scrollToElement(someElement, offset, duration);
+                    }, 10);
+                });
+
+
+            },
+            link: function (scope, element, attr) {
+
+
+            },
+            controllerAs: 'acSearchCtrl'
+        };
+    }
 })();
 
 
 WebFontConfig = {
-    google: {families: ['Futura:400,700:latin']}
+    google: {families: ['Open+Sans:400,700:latin']}
 };
 (function () {
     var wf = document.createElement('script');
